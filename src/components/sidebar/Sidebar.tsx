d0686877ef06d4eb3ce30cc7cd605221732343ae
Iram Navarrete <iram.navarrete@plumanegra.com.mx>
2023-06-08 14:48:30 -0600

    Tooltips sidebar added

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,25 +1,35 @@
 import { Link, useLocation } from 'react-router-dom';
 import { ReactElement } from 'react';
+import { Tooltip } from '@chakra-ui/react';
 import { EmailIcon, PagesIcon, SettingsIcon } from '../icons';
+import Theme from '../../constants/Theme';
 
 interface SidebarItem {
+  id: number,
   Icon: () => ReactElement,
+  label: string,
   path: string
 }
 
 const sidebarTopItems: SidebarItem[] = [
   {
+    id: 0,
     Icon: () => <PagesIcon />,
+    label: 'Pages',
     path: '/',
   },
   {
+    id: 1,
     Icon: () => <EmailIcon />,
+    label: 'Contact me',
     path: '/contact',
   },
 ];
 const sidebarBottomItems: SidebarItem[] = [
   {
+    id: 2,
     Icon: () => <SettingsIcon />,
+    label: 'Settings',
     path: '/settings',
   },
 ];
@@ -29,16 +39,28 @@ function Sidebar(): ReactElement {
 
   const renderIconsList = (iconList: SidebarItem[]): ReactElement => (
     <div className="flex flex-col">
-      {iconList.map(({ Icon, path }) => (
-        <Link
-          className={`${pathname === path ? 'bg-active-sidebar border-l-2 bg-active-sidebar border-accent' : ''}`}
-          to={path}
-          key={path}
+      {iconList.map(({
+        Icon, path, id, label,
+      }) => (
+        <Tooltip
+          key={`sidebarelement-top-${id}`}
+          placement="right"
+          hasArrow
+          label={label}
+          bg={Theme.colors.primary}
+          border="1px"
+          borderColor={Theme.colors.border}
+          arrowShadowColor={Theme.colors.border}
         >
-          <div className="flex flex-row justify-center py-2">
-            <Icon />
-          </div>
-        </Link>
+          <Link
+            className={`${pathname === path ? 'bg-active-sidebar border-l-2 bg-active-sidebar border-accent' : ''}`}
+            to={path}
+          >
+            <div className="flex flex-row justify-center py-2">
+              <Icon />
+            </div>
+          </Link>
+        </Tooltip>
       ))}
     </div>
   );
